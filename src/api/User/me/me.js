94046e7588b6778e162e429c60d20fa3c2cef439
sinbox0701 <psh090953@gmail.com>
2020-10-29 22:11:37 +0900

    me change

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -5,15 +5,7 @@ export default{
         me: async (_,__,{request,isAuthenticated}) => {
             isAuthenticated(request);
             const {user} = request;
-            ///
-            const userProfile = await prisma.user({id:user.id})
-            const posts = await prisma.user({id:user.id}).posts();
-            return {
-                user:userProfile,
-                posts
-            };
-            //return prisma.user({id:user.id}).$fragment(USER_FRAGMENT);
-            //fragment를 사용하면 fragement에 지정한 관계 아니면 데이터 불러올 수 X
+            return await prisma.user({id:user.id})
         }
     }
 };
